b52b13443a9ef750a4876425c8d272f9ecd38327
AlexanderARodin <rodin_aa@inbox.ru>
2022-02-21 13:43:57 +0300

    creation

--- Sources/RaaNWUtils/RaaNWUtils.swift
+++ Sources/RaaNWUtils/RaaNWUtils.swift
@@ -1,6 +1,44 @@
-public struct RaaNWUtils {
-    public private(set) var text = "Hello, World!"
+//
+//  NWUtils.swift
+//  tst_UDP
+//
+//  Created by the Dragon on 30.01.2022.
+//
 
-    public init() {
-    }
+import Foundation
+import Network
+
+//	//	//	//	//	//	//	//
+
+
+func sendData( _ data: Data?, on connection: NWConnection, informInMain: ((_ endpoint: NWEndpoint, _ error: NWError?) -> Void)? = nil) {
+	let endpoint = connection.endpoint
+	connection.send(content: data, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
+		DispatchQueue.main.async {
+			informInMain?(endpoint, NWError)
+		}
+	})))
+}
+
+func receiveData(on connection: NWConnection, informInMain: @escaping (_ endpoint: NWEndpoint, _ data: Data?, _ error: NWError?) -> Void) {
+	let endpoint = connection.endpoint
+	connection.receiveMessage { (data, context, isComplete, error) in
+		DispatchQueue.main.async {
+			informInMain(endpoint, data, error)
+		}
+	}
+}
+
+
+class MainCoder {
+	static private let encoder = JSONEncoder()
+	static private let decoder = JSONDecoder()
+	private init(){}
+	
+	static func decode<T>(_ type: T.Type, from data: Data) -> T? where T : Decodable {
+		try? decoder.decode(T.self, from: data)
+	}
+	static func encode<T>(_ value: T) -> Data? where T : Encodable {
+		try? encoder.encode(value)
+	}
 }
